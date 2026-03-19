d295282d6cac72bf9060ccf3b924ffc03d416ea2
fmarin27 <fernandomarin27@gmail.com>
2026-03-18 20:47:20 -0400

    Add Google Tag Manager to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from "next";
 import "./globals.css";
 import AppShell from "../components/AppShell";
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen">
         <AppShell>{children}</AppShell>
+        <GoogleTagManager gtmId="GTM-5N2Q6JKF" />
       </body>
     </html>
   );
